c6d9374be9749fa3c00e53e6dd7159df05a0fc20
Alex Ch <void5823@gmail.com>
2022-05-04 15:16:33 +0300

    Updated

--- Z3/index.php
+++ Z3/index.php
@@ -112,11 +112,10 @@ try {
 	//Подключение к базе данных. Подготовленный запрос. Не именованные метки.
 	$db = new PDO('mysql:dbname=u47480;host=localhost', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
   $db->exec("setnames utf8");
-
   $stmt = $db->prepare("INSERT INTO clientinfo (username, user_email, years, gender, userl, superpower, bio) VALUES (:username, :user_email, :years, :gender, :userl, :superpower, :bio)");
   $stmt -> execute(['username' => $user_name,'user_email' => $user_email, 'years' => $year,'gender' => $gender,'userl' => $user_l,'superpower' => $superpowers,'bio' => $bio]);
   $id = $db->lastInsertId();
-  echo "Данные успешно сохранены." . $id;
+  echo "Данные успешно сохранены. ID:" . $id;
 }
 catch(PDOException $e){
   //Если есть ошибка соединения, выводим её:

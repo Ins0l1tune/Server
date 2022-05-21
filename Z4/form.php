9da1b2bb8c65984d7c0ae8d0c6da80db9674c3e8
Alex Ch <void5823@gmail.com>
2022-05-21 12:27:12 +0300

    Add file

---
+++ Z4/form.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <style>
+/* ... */
+    </style>
+  </head>
+  <body>
+
+<?php
+if (!empty($messages)) {
+  print('<div id="messages">');
+  // Выводим все сообщения.
+  foreach ($messages as $message) {
+    print($message);
+  }
+  print('</div>');
+}
+
+// Далее выводим форму отмечая элементы с ошибками классом error
+// и задавая начальные значения элементов ранее сохраненными.
+?>
+
+    <form action="" method="POST">
+      <input name="fio" <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" />
+      <input type="submit" value="ok" />
+    </form>
+  </body>
+</html>

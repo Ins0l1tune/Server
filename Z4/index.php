4ac33d7f81e0459525e65576e64e3db433061f68
Alex Ch <void5823@gmail.com>
2022-05-25 15:58:21 +0300

    Updated

--- Z4/index.php
+++ Z4/index.php
@@ -8,6 +8,15 @@ header('Content-Type: text/html; charset=UTF-8');
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   // Массив для временного хранения сообщений пользователю.
   $messages = array();
+  $messages['username'] = '';
+  $messages['user_email'] = '';
+  $messages['years'] = '';
+  $messages['gender'] = '';
+  $messages['userl'] = '';
+  $messages['superpower'] = '';
+  $messages['bio'] = '';
+  $messages['usercheck'] = '';
+  $messages['data_saved'] = '';
 
   // Складываем признак ошибок в массив.
   $errors = array();
@@ -20,24 +29,96 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $errors['bio'] = !empty($_COOKIE['bio_err']);
   $errors['usercheck'] = !empty($_COOKIE['usercheck_err']);
   $errors['data_saved'] = !empty($_COOKIE['data_saved_err']);
-  // Складываем признак ошибок в массив.
-  $errors = array();
-  $errors['fio'] = !empty($_COOKIE['fio_error']);
-  // TODO: аналогично все поля.
 
-  // Выдаем сообщения об ошибках.
-  if ($errors['fio']) {
+  if ($errors['username'] == '1') {
     // Удаляем куку, указывая время устаревания в прошлом.
-    setcookie('fio_error', '', 100000);
+    setcookie('username_err', '', 100000);
     // Выводим сообщение.
-    $messages[] = '<div class="error">Заполните имя.</div>';
+    $messages['username'] = 'Укажите своё имя!';
+  } else if ($errors['username'] == '2') {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('username_err', '', 100000);
+    // Выводим сообщение.
+    $messages['username'] = 'Разрешены только буквенные символы!';
   }
-  // TODO: тут выдать сообщения об ошибках в других полях.
+
+  if ($errors['user_email'] == '1') {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('user_email_err', '', 100000);
+    // Выводим сообщение.
+    $messages['user_email'] = 'Email обязателен!';
+  } else if ($errors['user_email'] == '2') {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('user_email_err', '', 100000);
+    // Выводим сообщение.
+    $messages['user_email'] = 'Неверный формат электронной почты! Верный формат: primer85@test.com.';
+  }
+
+  if ($errors['years']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('years_err', '', 100000);
+    // Выводим сообщение.
+    $messages['years'] = 'Выберите год рождения';
+  }
+
+  if ($errors['gender']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('gender_error', '', 100000);
+    // Выводим сообщение.
+    $messages['gender'] = 'Заполните пол';
+  }
+
+  if ($errors['userl']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('userl_err', '', 100000);
+    // Выводим сообщение.
+    $messages['userl'] = 'Заполните число конечностей';
+  }
+
+  if ($errors['superpower']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('superpower_err', '', 100000);
+    // Выводим сообщение.
+    $messages['superpower'] = 'Нелегальная сверхспособность';
+  }
+
+
+  if ($errors['bio']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('bio_err', '', 100000);
+    // Выводим сообщение.
+    $messages['bio'] = 'Заполните биографию';
+  }
+
+  if ($errors['usercheck']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('usercheck_err', '', 100000);
+    // Выводим сообщение.
+    $messages['usercheck'] = 'Согласитесь';
+  }
+
+  if ($errors['data_saved']) {
+    // Удаляем куку, указывая время устаревания в прошлом.
+    setcookie('save_err', '', 100000);
+    $messages['data_saved'] = "Ошибка отправки: " . $_COOKIE['save_err'];
+  }
+
+  // Выдаем сообщение об успешном сохранении.
+  if (array_key_exists('save', $_GET) && $_GET['save']) {
+    // Если есть параметр save, то выводим сообщение пользователю.
+    $messages['data_saved'] = 'Спасибо, результаты сохранены.';
+  }
 
   // Складываем предыдущие значения полей в массив, если есть.
   $values = array();
-  $values['fio'] = empty($_COOKIE['fio_value']) ? '' : $_COOKIE['fio_value'];
-  // TODO: аналогично все поля.
+  $values['username'] = empty($_COOKIE['username_v']) ? '' : $_COOKIE['username_v'];
+  $values['user_email'] = empty($_COOKIE['user_email_v']) ? '' : $_COOKIE['user_email_v'];
+  $values['years'] = empty($_COOKIE['years_v']) ? '' : $_COOKIE['years_v'];
+  $values['gender'] = empty($_COOKIE['gender_v']) ? '' : $_COOKIE['gender_v'];
+  $values['userl'] = empty($_COOKIE['userl_v']) ? '' : $_COOKIE['userl_v'];
+  $superpower = empty($_COOKIE['superpower_v']) ? array() : json_decode($_COOKIE['superpower_v'], true);
+  $values['bio'] = empty($_COOKIE['bio_v']) ? '' : $_COOKIE['bio_v'];
+  $values['usercheck'] = empty($_COOKIE['usercheck_v']) ? '' : $_COOKIE['usercheck_v'];
 
   // Включаем содержимое файла form.php.
   // В нем будут доступны переменные $messages, $errors и $values для вывода 

b74f50ef9db0c4cace28b2843aa81524ab7db4cf
Alex Ch <void5823@gmail.com>
2022-05-27 10:23:04 +0300

    Updated

--- Z4/form.php
+++ Z4/form.php
@@ -11,6 +11,16 @@
 </head>
 
 <body>
+  	<?php
+	if (!empty($messages)) {
+		print('<div id="messages">');
+		// Выводим все сообщения.
+		foreach ($messages as $message) {
+			print($message);
+		}
+		print('</div>');
+	} 
+	?>
   <!-- Выводим форму отмечая элементы с ошибками классом error и задавая начальные значения элементов ранее сохраненными. -->
   <header>
     <div class="container">
